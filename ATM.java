d96a6690aa1d1b86afe54eda6cab7b4478277509
bluetwo4 <dalal.aditya.2456@gmail.com>
2025-07-03 17:31:01 +0530

    Delete ATM.java

--- ATM.java
+++
@@ -1,7 +0,0 @@
-public class ATM {
-    static void welcome() {
-        System.out.println("Hello, welcome to APNA BANK'S Portal.");
-        System.out.println("Please press 1 for withdrawal. \n Please press 2 for deposit. \n Please press 3 for checking your bank balance");
-    }
-
-}
